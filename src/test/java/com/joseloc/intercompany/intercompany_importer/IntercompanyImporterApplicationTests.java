ce0a82088cd4869ee92cf1eb8d72fcfb885ba5e5
José Nuñez Morocho <darksoul.js17@gmail.com>
2025-04-01 02:02:52 -0500

    first commit

---
+++ src/test/java/com/joseloc/intercompany/intercompany_importer/IntercompanyImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseloc.intercompany.intercompany_importer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntercompanyImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
