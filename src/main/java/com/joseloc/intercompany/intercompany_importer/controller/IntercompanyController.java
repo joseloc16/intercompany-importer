d4508232e2c21400cb0bf107e40019862131e9b0
José Nuñez Morocho <darksoul.js17@gmail.com>
2025-04-01 15:25:43 -0500

    modify endpoint in controller

--- src/main/java/com/joseloc/intercompany/intercompany_importer/controller/IntercompanyController.java
+++ src/main/java/com/joseloc/intercompany/intercompany_importer/controller/IntercompanyController.java
@@ -3,7 +3,8 @@ package com.joseloc.intercompany.intercompany_importer.controller;
 import com.joseloc.intercompany.intercompany_importer.model.dto.IntercompanyExcelDto;
 import com.joseloc.intercompany.intercompany_importer.service.IIntercompanyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,10 @@ public class IntercompanyController {
 
     private final IIntercompanyService service;
 
-    @GetMapping( value = "/get", headers = "Accept=application/json" )
-    public List<IntercompanyExcelDto> getAllCompanies( ) {
-        List<IntercompanyExcelDto> tasks = service.saveData( );
-        return tasks;
+    @PostMapping( value = "/import", headers = "Accept=application/json" )
+    public ResponseEntity<String> importIntercompanyData( ) {
+        List<IntercompanyExcelDto> importedData = service.saveData( );
+        return ResponseEntity.ok( "Se importaron " + importedData.size( ) + " registros correctamente." );
     }
 
 }
